7ce06d3ffca38b6bc9e1b0e6b4a8971e4a96258a
markatithinkbest <mark@ithinkbest.com>
2015-12-18 14:24:45 +0800

    change package id to com.twcloudwebapp.todo, need to modify google service json

---
+++ app/src/main/java/com/twcloudwebapp/todo/ShoppingListApplication.java
@@ -0,0 +1,17 @@
+package com.twcloudwebapp.todo;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /* Initialize Firebase */
+        Firebase.setAndroidContext(this);
+    }
+
+}
---
+++ app/src/main/java/com/twcloudwebapp/todo/utils/Utils.java
@@ -0,0 +1,43 @@
+package com.twcloudwebapp.todo.utils;
+
+import android.content.Context;
+
+import com.twcloudwebapp.todo.model.ShoppingList;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * Utility class
+ */
+public class Utils {
+    /**
+     * Format the timestamp with SimpleDateFormat
+     */
+    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private Context mContext = null;
+
+
+    /**
+     * Public constructor that takes mContext for later use
+     */
+    public Utils(Context con) {
+        mContext = con;
+    }
+
+    /**
+     * Return true if currentUserEmail equals to shoppingList.owner()
+     * Return false otherwise
+     */
+    public static boolean checkIfOwner(ShoppingList shoppingList, String currentUserEmail) {
+        return (shoppingList.getOwner() != null &&
+                shoppingList.getOwner().equals(currentUserEmail));
+    }
+
+    /**
+     * Encode user email to use it as a Firebase key (Firebase does not allow "." in the key name)
+     * Encoded email is also used as "userEmail", list and item "owner" value
+     */
+    public static String encodeEmail(String userEmail) {
+        return userEmail.replace(".", ",");
+    }
+}
